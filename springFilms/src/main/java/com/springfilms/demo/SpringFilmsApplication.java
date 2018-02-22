00d76f50f550f2b01305adb60a89d668cfe7a703
Carlos Ochavo <acochavosdi@gmail.com>
2018-02-22 11:32:43 +0100

    Subida del proyecto base
    - Estructura de carpetas / paquetes
    - Configuracion base de datos a traves de DatabaseConfig.java + application.properties
    - Configuracion de rutas internas de Spring a traves de WebConfig.java + application.properties
    - Añadadidas dependencias básicas en pom.xml

---
+++ springFilms/src/main/java/com/springfilms/demo/SpringFilmsApplication.java
@@ -0,0 +1,12 @@
+package com.springfilms.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFilmsApplication.class, args);
+	}
+}
