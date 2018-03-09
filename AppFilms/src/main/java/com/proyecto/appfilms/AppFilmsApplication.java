e3e510801a0d7e0f33f628d29a1cfb84b15561ec
jotaemece3 <jmca_95@hotmail.com>
2018-03-09 13:51:53 +0100

    #5 - Aplicación terminada con configuración para despliegue en Tomcat

--- AppFilms/src/main/java/com/proyecto/appfilms/AppFilmsApplication.java
+++ AppFilms/src/main/java/com/proyecto/appfilms/AppFilmsApplication.java
@@ -2,11 +2,21 @@ package com.proyecto.appfilms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class AppFilmsApplication {
+public class AppFilmsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppFilmsApplication.class, args);
 	}
+	/**
+	 * Esto lo añadimos para poder desplegarlo en Tomcat
+	 */
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AppFilmsApplication.class);
+    }
 }
