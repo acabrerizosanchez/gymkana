f97524040cabb683c38185e0b8f5701bd4928079
amrrus <antoniomanuel.r.r@gmail.com>
2018-05-30 11:55:48 +0200

    @amrrus - #Integrity - GET request changed to POST request

---
+++ RatingApp/src/main/java/com/rating/app/RatingAppApplication.java
@@ -0,0 +1,12 @@
+package com.rating.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingAppApplication.class, args);
+	}
+}
---
+++ RatingApp/src/main/java/com/rating/app/movieRepository.java
@@ -0,0 +1,7 @@
+package com.rating.app;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface movieRepository extends CrudRepository<Movie, Integer> {
+
+}
